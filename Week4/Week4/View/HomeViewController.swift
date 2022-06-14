aa7dac5869f7873819d10e7b5b0669400e6c34e4
Semra <karadogansemra@gmail.com>
2022-06-14 10:02:58 +0300

    Add views

---
+++ Week4/Week4/View/HomeViewController.swift
@@ -0,0 +1,37 @@
+//
+//  HomeViewController.swift
+//  Week4
+//
+//  Created by Semra KARADOĞAN on 14.06.2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var homePageControl: UIPageControl!
+    @IBOutlet weak var homeCollectionView: UICollectionView!
+    @IBOutlet weak var homeScrollView: UIScrollView!
+   
+    @IBOutlet weak var homeScrollTitleLabel: UILabel!
+    
+    @IBOutlet weak var homeScrollCashLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Week4/Week4/View/MarketsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MarketsViewController.swift
+//  Week4
+//
+//  Created by Semra KARADOĞAN on 14.06.2022.
+//
+
+import UIKit
+
+class MarketsViewController: UIViewController {
+
+    @IBOutlet weak var marketsTableView: UITableView!
+    
+    @IBOutlet weak var marketsSearchBar: UISearchBar!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
